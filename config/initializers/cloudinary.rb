8300414f876b6316f35c0a05e6b5534a146f37a3
Hrag Chanchanian <hragatron@gmail.com>
2015-07-31 18:24:41 -0700

    some design stuff for mobile done

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+  config.cloud_name =	'hlrlmfh5p'
+  config.api_key =	'219332847933429'
+  config.api_secret =	'FmbxN_w9OJsh06cBHVPIza2QqGQ'
+  config.cdn_subdomain = true
+end
+
